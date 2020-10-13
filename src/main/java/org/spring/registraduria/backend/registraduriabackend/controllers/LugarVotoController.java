a64ec354e3d707399d6843f972bdccd5e3880a5f
Christian Chacon <christian_kazuma@hotmail.com>
2020-10-13 14:32:00 -0500

    Se crean los controladores y los metodos para obtener la lista de Lugares y Mesas

--- src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/LugarVotoController.java
+++ src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/LugarVotoController.java
@@ -1,5 +1,26 @@
 package org.spring.registraduria.backend.registraduriabackend.controllers;
 
+import java.util.List;
+
+import org.spring.registraduria.backend.registraduriabackend.model.entities.TablaLugarVoto;
+import org.spring.registraduria.backend.registraduriabackend.model.services.ILugarVotoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "http://localhost:4200" })
+@RestController
+@RequestMapping("/api")
 public class LugarVotoController {
+
+    @Autowired
+    private ILugarVotoService lugarVotoService;
+
+    @GetMapping("/lugares")
+    public List<TablaLugarVoto> index(){
+        return lugarVotoService.findAll();
+    }
     
 }
--- src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/MesaController.java
+++ src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/MesaController.java
@@ -1,5 +1,26 @@
 package org.spring.registraduria.backend.registraduriabackend.controllers;
 
+import java.util.List;
+
+import org.spring.registraduria.backend.registraduriabackend.model.entities.TablaMesa;
+import org.spring.registraduria.backend.registraduriabackend.model.services.IMesaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = { "http://localhost:4200" })
+@RestController
+@RequestMapping("/api")
 public class MesaController {
     
+    @Autowired
+    private IMesaService mesaService;
+
+    @GetMapping("/mesas")
+    public List<TablaMesa> index(){
+        return mesaService.findAll();
+    }
+
 }
