c7709e03606f5aa852b3402ddb3ee71cc43844cf
christian chacon <christian_kazuma@hotmail.com>
2022-01-30 11:34:02 -0500

    Se implementa el metodo update en la funcionalidad Departamento

--- src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/DepartamentoRestController.java
+++ src/main/java/org/spring/registraduria/backend/registraduriabackend/controllers/DepartamentoRestController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,6 +60,14 @@ public class DepartamentoRestController {
         tablaDepartamento.setCodDepartamento(departamento.getCodDepartamento());
         tablaDepartamento.setDepartamento(departamento.getDepartamento());
         return departamentoService.create(tablaDepartamento);
+    }
+
+    @PutMapping("/departamento/{codDepartamento}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TablaDepartamento update(@RequestBody DepartamentoDto departamentoDto, @PathVariable String codDepartamento){
+        TablaDepartamento departamento=departamentoService.findById(codDepartamento);
+        departamento.setDepartamento(departamentoDto.getDepartamento());
+        return departamentoService.create(departamento);
     }
 
 }
